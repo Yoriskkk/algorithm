d92c033df9242a56490dd8143758acda68647b95
Yoriskkk <379805167@qq.com>
2020-05-14 01:49:06 +0800

    面试题11. 旋转数组的最小数字

---
+++ src/main/java/com/algorithm/array/MinArray.java
@@ -0,0 +1,14 @@
+package com.algorithm.array;
+
+public class MinArray {
+
+    public int minArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i]<numbers[0]){
+                return numbers[i];
+            }
+        }
+        return numbers[0];
+    }
+
+}
