851c6efaba49d0d70d67dac3c1fd44c0387e7f34
Yoriskkk <379805167@qq.com>
2020-05-18 01:40:34 +0800

    面试题10- I. 斐波那契数列

--- src/main/java/com/algorithm/array/Fib.java
+++ src/main/java/com/algorithm/array/Fib.java
@@ -8,23 +8,23 @@ public class Fib {
 
     public static int fib(int n) {
         memo = new int[101];
-        for (int i = 0; i < n+1; i++) {
+        for (int i = 0; i < n + 1; i++) {
             memo[i] = -1;
         }
 
         return func(n);
     }
 
-    public static int func(int n){
-        if(n == 1){
+    public static int func(int n) {
+        if (n == 1) {
             return 1;
         }
-        if(n == 0){
+        if (n == 0) {
             return 0;
         }
         int result = memo[n];
-        if(result == -1){
-            memo[n] = func(n-1)+func(n-2);
+        if (result == -1) {
+            memo[n] = func(n - 1) + func(n - 2);
         }
         return memo[n];
     }
@@ -33,6 +33,6 @@ public class Fib {
         long start = System.currentTimeMillis();
         System.out.println(fib(48));
         long end = System.currentTimeMillis();
-        System.out.println((long)end-start);
+        System.out.println((long) end - start);
     }
 }
