93c934492a65fdff7778c6826b8bc60bdc692aa8
Yoriskkk <379805167@qq.com>
2020-05-20 23:49:37 +0800

    894. 所有可能的满二叉树 未解

---
+++ src/main/java/com/algorithm/tree/AllPossibleFBT.java
@@ -0,0 +1,17 @@
+package com.algorithm.tree;
+
+import java.util.List;
+
+/**
+ * #894	所有可能的满二叉树
+ */
+public class AllPossibleFBT {
+
+    public List<TreeNode> allPossibleFBT(int N) {
+
+        //如果n>=3
+        return null;
+
+    }
+
+}
