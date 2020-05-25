db12b53e73573d57c6ad8cbe93b32c84e1714bc9
Yoriskkk <379805167@qq.com>
2020-05-25 12:34:22 +0800

    106. 从中序与后序遍历序列构造二叉树 未解

---
+++ src/main/java/com/algorithm/tree/BuildTree.java
@@ -0,0 +1,23 @@
+package com.algorithm.tree;
+
+/**
+ * 106. 从中序与后序遍历序列构造二叉树
+ * 通过数组的排序，构造对应的二叉树
+ */
+public class BuildTree {
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+        return dfs(inorder,postorder);
+
+    }
+
+    private TreeNode dfs(int[] inorder, int[] postorder) {
+
+
+
+
+        return null;
+    }
+
+}
