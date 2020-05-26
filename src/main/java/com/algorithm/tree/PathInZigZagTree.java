b2f699b1880b4cb81076506eb7a8e462cb05630b
Yoriskkk <379805167@qq.com>
2020-05-26 12:09:36 +0800

    1104. 二叉树寻路 未解

---
+++ src/main/java/com/algorithm/tree/PathInZigZagTree.java
@@ -0,0 +1,16 @@
+package com.algorithm.tree;
+
+import java.util.List;
+
+/**
+ * 1104. 二叉树寻路
+ */
+public class PathInZigZagTree {
+
+    public List<Integer> pathInZigZagTree(int label) {
+
+        return null;
+
+    }
+
+}
