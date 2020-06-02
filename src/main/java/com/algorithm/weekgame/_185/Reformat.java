933835ab48d63aaa7e1ee9b0e8e466442b7e4550
Yoriskkk <379805167@qq.com>
2020-06-02 20:09:06 +0800

    1417. 重新格式化字符串 总结

--- src/main/java/com/algorithm/weekgame/_185/Reformat.java
+++ src/main/java/com/algorithm/weekgame/_185/Reformat.java
@@ -8,19 +8,22 @@ import java.util.Queue;
  */
 public class Reformat {
 
+    /**
+     * 总结：
+     * 1.对于数字字符的判断0的asc码是48, 9是57
+     * 2.对于尾节点的添加需要判断队列中是否还有值
+     * @param s
+     * @return
+     */
     public static String reformat(String s) {
         char[] chars = s.toCharArray();
-        int numSize = 0;
-        int chrSize = 0;
         Queue<Character> queueChr = new LinkedList();
         Queue<Character> queueNum = new LinkedList();
         for (int i = 0; i < chars.length; i++) {
             int chr = chars[i];
             if(chr<48 || chr>57){
-                chrSize++;
                 queueChr.add(chars[i]);
             }else {
-                numSize++;
                 queueNum.add(chars[i]);
             }
         }
@@ -36,7 +39,6 @@ public class Reformat {
                 if(queueNum.size()!=0){
                     stringBuilder.append(queueNum.remove());
                 }
-
             }
         }else {
             int size = queueChr.size();
@@ -51,8 +53,13 @@ public class Reformat {
     }
 
     public static void main(String[] args) {
-        String str = "a0b1c2";
-        reformat(str);
+        String str = "0123456789";
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+
+            int chr = chars[i];
+            System.out.println(chr);
+        }
 
     }
 
