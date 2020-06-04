1831c56db0c8ca6ea84d6ef36b8fcd95689bb787
Yoriskkk <379805167@qq.com>
2020-06-05 00:11:14 +0800

    515. 在每个树行中找最大值

---
+++ src/main/java/com/algorithm/tree/LargestValues.java
@@ -0,0 +1,36 @@
+package com.algorithm.tree;
+
+import java.util.*;
+
+public class LargestValues {
+
+    public List<Integer> largestValues(TreeNode root) {
+        if(root == null){
+            return Collections.emptyList();
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while(queue.size()!=0){
+            int size =queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+
+                TreeNode remove = queue.remove();
+                if(remove.val>max){
+                    max = remove.val;
+                }
+                if(remove.left!=null){
+                    queue.add(remove.left);
+                }
+                if(remove.right!=null){
+                    queue.add(remove.right);
+                }
+            }
+            list.add(max);
+
+        }
+        return list;
+    }
+
+}
