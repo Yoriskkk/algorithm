b9046292ba4198d0cdf548dd911eeef33823d47f
Yoriskkk <379805167@qq.com>
2020-07-01 01:47:44 +0800

    80. 删除排序数组中的重复项||

--- src/main/java/com/algorithm/array/RemoveDuplicates.java
+++ src/main/java/com/algorithm/array/RemoveDuplicates.java
@@ -18,4 +18,28 @@ public class RemoveDuplicates {
         return i;
     }
 
+    public int removeDuplicates2(int[] nums) {
+
+        int i = 2;
+        if(nums.length < i){
+            return nums.length;
+        }
+        for (int j = 2; j < nums.length; j++) {
+
+            if(nums[j] != nums[j+2] && nums[j] != nums[j+1]){
+                nums[i] = nums[j];
+                i ++;
+                continue;
+            }
+
+            if(nums[j] != nums[j+2] && nums[j] == nums[j+1]){
+                nums[i] = nums[j];
+                nums[i+1] = nums[j+1];
+                i += 2;
+                continue;
+            }
+        }
+        return i;
+    }
+
 }
