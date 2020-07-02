bc979acda6d7fab1d33306e5d60dfe452782f142
Yoriskkk <379805167@qq.com>
2020-07-02 13:29:24 +0800

    41. 缺失的第一个正数

---
+++ src/main/java/com/algorithm/array/FirstMissingPositive.java
@@ -0,0 +1,22 @@
+package com.algorithm.array;
+
+import java.util.*;
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+        int length = nums.length;
+        //大致的思路，将数据元素放入hashset中，然后一个一个contains
+        HashSet set = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        for (int i = 1; i < length; i++) {
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return length+1;
+    }
+
+}
