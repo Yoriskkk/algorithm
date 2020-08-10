501296adcb0824f859043cd458f5fec037dce5a7
Yoriskkk <379805167@qq.com>
2020-08-10 20:40:54 +0800

    20. 有效的括号

--- src/main/java/com/algorithm/array/MaxArea.java
+++ src/main/java/com/algorithm/array/MaxArea.java
@@ -22,10 +22,70 @@ public class MaxArea {
         return max;
     }
 
+
+
+
+
+    public static int maxArea1(int[] height) {
+        int right = height.length - 1;
+        int maxArea = 0;
+        for (int left = 0; left < right;) {
+            int area = getArea(height, right, left);
+            maxArea = Math.max(maxArea,area);
+            if(height[left]<height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+
+
+
+
+
+
+
+
+
+    public static int maxArea2(int[] height){
+        //核心思路，使用left和right两个指针，左右收敛进行计算最大面积
+        int left = 0;
+        int right = height.length-1;
+        int maxArea = 0;
+        int area = 0;
+        while(left<right){
+            area = getArea(height,height[right],height[left]);
+            maxArea = Math.max(maxArea,area);
+            if(height[left]<height[right]){
+                left++;
+            }else if(height[left]==height[right]){
+                break;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+
+
+
+
+
+
+
+    private static int getArea(int[] height, int right, int left) {
+        return (right-left)*Math.min(height[left],height[right]);
+    }
+
+
     public static void main(String[] args) {
 
         int[] nums = new int[]{1,8,6,2,5,4,8,3,7};
-        maxArea(nums);
+        maxArea2(nums);
 
 
     }
