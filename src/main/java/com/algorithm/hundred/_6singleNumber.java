fb6cf083699b170b168d0a2b4a6175a65a2bad3b
Yoriskkk <379805167@qq.com>
2020-09-07 13:58:24 +0800

    100热题06.只出现一次的数字

--- src/main/java/com/algorithm/hundred/_6singleNumber.java
+++ src/main/java/com/algorithm/hundred/_6singleNumber.java
@@ -28,7 +28,7 @@ public class _6singleNumber {
 
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
-            result = result ^ nums[i];
+            result  ^= nums[i];
         }
         return result;
     }
---
+++ src/main/java/com/algorithm/hundred/_7majorElement.java
@@ -0,0 +1,21 @@
+package com.algorithm.hundred;
+
+/**
+ * 169.多数元素
+ * 给定一个大小为n的数组，找到其中的多数元素，多数元素是指在数组中出现次数大于[n/2]的元素
+ * 示例 1:
+ *
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ */
+public class _7majorElement {
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+
+
+}
