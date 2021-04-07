8745b06612d68a31e11f0a073c683c4b7587ae13
Yoriskkk <379805167@qq.com>
2021-04-07 12:37:29 +0800

    剑指 Offer 58 - I. 翻转单词顺序

---
+++ src/main/java/com/wz/week_1/ReverseWords.java
@@ -0,0 +1,38 @@
+package com.wz.week_1;
+
+/**
+ *
+ */
+public class ReverseWords {
+
+    public static String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        //对字符串的首尾去掉空格，针对示例2
+        s = s.trim();
+        //1.首先对字符串做分割处理
+        String[] split = s.split(" ");
+        //2.对多余空格进行处理
+        for (int i = split.length-1; i >=0; i--) {
+            if(split[i].contains(" ")){
+                split[i] = split[i].replaceAll(" ", "");
+            }
+            if(split[i].equals("")){
+                continue;
+            }else {
+                if(i!=0){
+                    result.append(split[i]+" ");
+                }else if(i == 0){
+                    result.append(split[i]);
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String str = "a good   example";
+        reverseWords(str);
+    }
+
+}
